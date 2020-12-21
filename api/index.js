665a6c4cc8d64102e44eca9e92d72ce754ce117d
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-12-21 23:22:27 +0800

    ping

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.get('/ping', (req, res) => {
+  res.send('PING!');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);

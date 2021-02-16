cb9dea69fd20bfb74ba5ec11350cf459911983d6
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-02-16 13:30:14 +0800

    cypress env

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,7 +12,7 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 const dotenv = require('dotenv');
-dotenv.config({ path: '../../.env' });
+dotenv.config();
 const APP_SECRET = process.env.APP_SECRET;
 console.log({ APP_SECRET });
 /**

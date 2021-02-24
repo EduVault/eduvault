580ba3d80020442b1864ff26b7356be4f2610583
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-02-24 12:37:45 +0800

    e2e

--- cypress/integration/flashcards.js
+++ cypress/integration/flashcards.js
@@ -5,7 +5,8 @@ const APP_SECRET = Cypress.env('APP_SECRET');
 
 describe('Flashcards Suite', async () => {
   dropCollections(APP_SECRET);
-
+  localStorage.clear();
+  indexedDB.deleteDatabase('eduvault');
   it('logs in', () => {
     cy.visit(exampleURL);
 
@@ -29,29 +30,27 @@ describe('Flashcards Suite', async () => {
   });
   it('can create a card', () => {
     cy.get('.new-card-btn').click();
-    cy.get('.ql-editor > p').first().focus();
-    cy.get('.form__text-input').type('Test Deck');
+    cy.get('.ql-editor > p').first().type('Test card front');
+    cy.get('.ql-editor > p').last().type('Test card back');
 
-    cy.get('.form__text-input[name=card-front-input]').type('Test card front');
-    cy.get('.form__text-input[name=card-back-input]').type('Test card back');
-    cy.get('.card-input__add-icon').click();
+    cy.get('.form__button--confirm').click();
 
-    cy.contains('.flashcard__front > .flashcard__content', 'Test card front');
-    cy.contains('.flashcard__back > .flashcard__content', 'Test card back');
+    cy.contains('Test card front');
+    cy.contains('Test card back');
   });
   it('can edit a card', () => {
     cy.get('.deck-display__buttons-col > .buttons-col__button--edit').eq(0).click();
-    cy.get('.card-editor .form__text-input[name=card-front-input]').type('Test card edit front');
-    cy.get('.card-editor .form__text-input[name=card-back-input]').type('Test card edit back');
-    cy.get('.card-editor .form__button--confirm').click();
-    cy.contains('.flashcard__front > .flashcard__content', 'Test card edit front');
-    cy.contains('.flashcard__back > .flashcard__content', 'Test card edit back');
+    cy.get('.ql-editor').first().type('Test card edit front');
+    cy.get('.ql-editor').last().type('Test card edit back');
+    cy.get('.form__button--confirm').click();
+    cy.contains('Test card edit front');
+    cy.contains('Test card edit back');
   });
   it('cancels edit when cancel is clicked', () => {
     cy.get('.deck-display__buttons-col > .buttons-col__button--edit').eq(0).click();
-    cy.get('.card-editor .form__text-input[name=card-front-input]').type('Test card edit front');
-    cy.get('.card-editor .form__text-input[name=card-back-input]').type('Test card edit back');
-    cy.get('.card-editor .form__button--cancel').click();
+    cy.get('.ql-editor').first().type('Test card front');
+    cy.get('.ql-editor').last().type('Test card back');
+    cy.get('.form__button--cancel').click();
     cy.contains('.flashcard__front', 'What is a group of zebra').should(
       'not.contain.text',
       'Test card edit front',
@@ -59,22 +58,23 @@ describe('Flashcards Suite', async () => {
     cy.contains('.flashcard__back', 'A zeal').should('not.contain.text', 'Test card edit back');
   });
   it('can delete a card', () => {
+    cy.contains('.flashcard__front', 'What is a group of zebra');
     cy.get('.deck-display__buttons-col > .buttons-col__button--delete').eq(0).click();
     cy.contains('.flashcard__front', 'What is a group of zebra').should('not.exist');
   });
   it('can flip a card when clicked', () => {
-    cy.contains('.flashcard__front', 'What is a group of zebra').should('be.visible');
+    cy.contains('.flashcard__front', 'What is a group of slugs').should('be.visible');
     cy.get('.flashcard__front').eq(0).click();
-    cy.contains('.flashcard__front', 'What is a group of zebra').should('not.be.visible');
-    cy.contains('.flashcard__back', 'A zeal').should('be.visible');
+    cy.contains('.flashcard__front', 'What is a group of slugs').should('not.be.visible');
+    cy.contains('.flashcard__back', 'A cornucopia').should('be.visible');
     cy.get('.flashcard__back').eq(0).click();
-    cy.contains('.flashcard__back', 'A zeal').should('not.be.visible');
+    cy.contains('.flashcard__back', 'A cornucopia').should('not.be.visible');
   });
   it('saves state on page reload', () => {
-    cy.contains('.flashcard__front', 'What is a group of zebra').should('exist');
+    cy.contains('.flashcard__front', 'What is a group of slugs').should('exist');
     cy.get('.deck-display__buttons-col > .buttons-col__button--delete').eq(0).click();
-    cy.contains('.flashcard__front', 'What is a group of zebra').should('not.exist');
+    cy.contains('.flashcard__front', 'What is a group of slugs').should('not.exist');
     cy.reload();
-    cy.contains('.flashcard__front', 'What is a group of zebra').should('not.exist');
+    cy.contains('.flashcard__front', 'What is a group of slugs').should('not.exist');
   });
 });

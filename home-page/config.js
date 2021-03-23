614598df3e0abcf36df9a2ee06ca616e6106ab0e
jewcub <xiaokourou@gmail.com>
2021-03-23 14:54:46 +0000

    staging deploy

--- home-page/config.js
+++ home-page/config.js
@@ -1,4 +1,3 @@
-export const EDUVAULT_APP_URL = 'https://app.' + process.env.VUE_APP_PROD_HOST
+export const EDUVAULT_APP_URL = 'http://app.' + process.env.VUE_APP_PROD_HOST
 
-
-export const EXAMPLE_APP_URL = 'https://example.' + process.env.VUE_APP_PROD_HOST
+export const EXAMPLE_APP_URL = 'http://example.' + process.env.VUE_APP_PROD_HOST

f81edc34225fe397f89a2be10d45067714fe16d6
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-05-22 18:57:43 +0800

    nightwatch e2e from cypress, happy path test

---
+++ e2e/index.ts
@@ -0,0 +1,22 @@
+import { NightwatchBrowser } from 'nightwatch';
+
+export const HappyPath = (browser: NightwatchBrowser) => {
+  browser
+    .url('https://home.localhost')
+    .waitForElementPresent('h1[data-testid="eduvault-title"]')
+    .assert.containsText('h1', 'EDUVAULT')
+    .click('button[data-testid="button-try-now"]');
+  browser.expect.element('a[data-testid="link-example"]').is.visible;
+  browser.click('a[data-testid="link-example"]');
+  // navigates to example
+  browser.waitForElementVisible('img.eduvault-button');
+  browser.click('img.eduvault-button');
+  browser.waitForElementPresent('input[type=email]');
+  browser.setValue('input[type=email]', 'example@somewhere.com');
+  browser.setValue('input[type=password]', 'Password123');
+  browser.click('button[type=submit]');
+  // logs in
+  browser.waitForElementPresent('h2[data-testid="deck-title"]', 10000);
+  // starts DB and shows default deck
+  browser.expect.element('h2[data-testid="deck-title"]').text.to.contain('Default Deck');
+};
---
+++ nightwatch.conf.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line camelcase
+const src_folders = ["test-build"];
+const webdriver = {
+  start_process: true,
+  server_path: "node_modules/.bin/chromedriver",
+  port: 9515
+};
+
+const headed = {
+  src_folders,
+  webdriver,
+  test_settings: {
+    default: {
+      desiredCapabilities: {
+        browserName: "chrome"
+      }
+    }
+  }
+};
+
+const headless = {
+  src_folders,
+  webdriver,
+  test_settings: {
+    default: {
+      desiredCapabilities: {
+        browserName: "chrome",
+        chromeOptions: {
+          args: ["headless"]
+        }
+      }
+    }
+  }
+};
+
+const config = process.env.GITHUB_ACTIONS ? headless : headed;
+module.exports = config;

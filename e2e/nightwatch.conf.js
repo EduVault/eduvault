e31bdc090288baf1da581d93ab505ae5a0b1be54
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-05-24 15:28:03 +0800

    nightwatch docker

--- e2e/nightwatch.conf.js
+++ e2e/nightwatch.conf.js
@@ -5,21 +5,11 @@ module.exports =
   "output_folder": "./test/reports",
   "custom_assertions_path": "",
   // "globals_path": "test/globals",
-  "selenium": {
-    "start_process": false,
-    "server_path": "/opt/selenium/selenium-server-standalone.jar",
-    "log_path": "test/reports",
-    "host": "127.0.0.1",
-    "port": 4444,
-    "cli_args": {
-      "webdriver.chrome.driver": "/opt/selenium/chromedriver-2.29"
-    }
-  },
   "test_settings": {
     "default": {
-      "selenium_port": 4444,
+      "selbenium_port": 4444,
       "selenium_host": "localhost",
-      "silent": true,
+      "silent": false,
       "desiredCapabilities": {
         "javascriptEnabled": true,
         "acceptSslCerts": true,

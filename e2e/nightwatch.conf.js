2fcee28c1e97bdc2604035b5fc1a45708c1078b1
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-05-24 21:22:33 +0800

    back to original strat

--- e2e/nightwatch.conf.js
+++ e2e/nightwatch.conf.js
@@ -1,24 +1,42 @@
-const HEADLESS = process.env.GITHUB_ACTIONS || process.env.HEADLESS_E2E === '1' ? true : false
-const DOCKER_COMPOSE = process.env.DOCKER_COMPOSE === '1'
-module.exports =
-{
-  "src_folders": ["./test-build"],
-  "output_folder": "./test/reports",
-  "custom_assertions_path": "",
-  // "globals_path": "test/globals",
-  "test_settings": {
-    "default": {
-      "selbenium_port": 4444,
-      "selenium_host": DOCKER_COMPOSE ? 'selenium-hub' : "localhost",
-      "silent": false,
-      "desiredCapabilities": {
-        "javascriptEnabled": true,
-        "acceptSslCerts": true,
-        "browserName": "chrome",
-        "chromeOptions": {
-          "args": HEADLESS ? ["headless", "no-sandbox", "disable-gpu"] : []
+// eslint-disable-next-line camelcase
+const src_folders = ["./test-build"];
+const webdriver = {
+  start_process: true,
+  server_path: "node_modules/.bin/chromedriver",
+  port: 9515
+};
+
+const desiredCapabilities = {
+  browserName: "chrome",
+  acceptSslCerts: true,
+  acceptInsecureCerts: true,
+  javascriptEnabled: true,
+}
+
+const headed = {
+  src_folders,
+  webdriver,
+  test_settings: {
+    default: {
+      desiredCapabilities
+    }
+  }
+};
+
+const headless = {
+  src_folders,
+  webdriver,
+  test_settings: {
+    default: {
+      desiredCapabilities: {
+        ...desiredCapabilities,
+        chromeOptions: {
+          args: ["--headless", "--no-sandbox", "--ignore-certificate-errors"]
         }
       }
     }
   }
-}
+};
+
+const config = !!process.env.GITHUB_ACTIONS || process.env.HEADLESS_E2E === '1' ? headless : headed;
+module.exports = config;

fc3f971a11d1764df6a0042a959d691e00b3f619
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-05-24 15:05:16 +0800

    new approach - use selenium images

--- e2e/nightwatch.conf.js
+++ e2e/nightwatch.conf.js
@@ -1,44 +1,33 @@
-// eslint-disable-next-line camelcase
-const src_folders = ["./test-build"];
-const webdriver = {
-  start_process: true,
-  server_path: "node_modules/.bin/chromedriver",
-  port: 9515
-};
-
-const desiredCapabilities = {
-
-  browserName: "chrome",
-  acceptSslCerts: true,
-  acceptInsecureCerts: true,
-  javascriptEnabled: true,
-
-}
-
-const headed = {
-  src_folders,
-  webdriver,
-  test_settings: {
-    default: {
-      desiredCapabilities
+const HEADLESS = process.env.GITHUB_ACTIONS || process.env.HEADLESS_E2E ? true : false
+module.exports =
+{
+  "src_folders": ["./test-build"],
+  "output_folder": "./test/reports",
+  "custom_assertions_path": "",
+  // "globals_path": "test/globals",
+  "selenium": {
+    "start_process": false,
+    "server_path": "/opt/selenium/selenium-server-standalone.jar",
+    "log_path": "test/reports",
+    "host": "127.0.0.1",
+    "port": 4444,
+    "cli_args": {
+      "webdriver.chrome.driver": "/opt/selenium/chromedriver-2.29"
     }
-  }
-};
-
-const headless = {
-  src_folders,
-  webdriver,
-  test_settings: {
-    default: {
-      desiredCapabilities: {
-        ...desiredCapabilities,
-        chromeOptions: {
-          args: ["--headless", "--no-sandbox", "--ignore-certificate-errors"]
+  },
+  "test_settings": {
+    "default": {
+      "selenium_port": 4444,
+      "selenium_host": "localhost",
+      "silent": true,
+      "desiredCapabilities": {
+        "javascriptEnabled": true,
+        "acceptSslCerts": true,
+        "browserName": "chrome",
+        "chromeOptions": {
+          "args": HEADLESS ? ["headless", "no-sandbox", "disable-gpu"] : []
         }
       }
     }
   }
-};
-
-const config = process.env.GITHUB_ACTIONS || process.env.HEADLESS_E2E ? headless : headed;
-module.exports = config;
+}

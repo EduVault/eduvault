a1f70b621668ac8e5c0c31a04d478dbf1bf52ee0
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-07-15 10:31:51 +0800

    e2e

--- e2e/tests/index.ts
+++ e2e/tests/index.ts
@@ -2,7 +2,7 @@ import { NightwatchAPI, NightwatchBrowser, NightwatchCallbackResult } from 'nigh
 const serverTestEnv = process.env.TEST === '1';
 console.log({ serverTestEnv, 'process.env.TEST': process.env.TEST });
 
-export const Base = (browser: NightwatchBrowser)=>{
+export const Base = (browser: NightwatchBrowser) => {
   // check nightwatch is working
   // browser.url('https://google.com').waitForElementPresent('body').assert.titleContains('Google');
   // check base url working
@@ -11,17 +11,10 @@ export const Base = (browser: NightwatchBrowser)=>{
   // browser
   // seems that on the CI headless browser they don't like URLs with one dot?
   //   .url('http://localhost')
-  //   .waitForElementPresent('body', 120000, false)
   //   .waitForElementPresent('h1[data-testid="eduvault-title"]', 120000, false, () =>
   //     console.log('checking root'),
   //   )
 
-  browser
-    .url('http://home.localhost')
-    .waitForElementPresent('body')
-    .waitForElementPresent('h1[data-testid="eduvault-title"]', 120000, false, () =>
-      console.log('checking home'),
-    );
   // function (this: NightwatchAPI, result: NightwatchCallbackResult<void>) {
   //   console.log({ browser: this, result });
   //   this.getText('body', function (result) {
@@ -35,38 +28,42 @@ export const Base = (browser: NightwatchBrowser)=>{
     .waitForElementVisible('img.eduvault-button', 50000, false, () =>
       console.log('checking example'),
     )
-    .waitForElementPresent('a[href^="http://app.localhost"]', 50000, false);
   browser
     .url('http://app.localhost')
     .waitForElementPresent('input[type=email]', 50000, false, () => console.log('checking app'));
-}
-
-export const HappyPath = (browser: NightwatchBrowser) => {
-
   browser
     .url('http://home.localhost')
-    .waitForElementPresent('body', 120000, false)
+    .waitForElementPresent('h1[data-testid="eduvault-title"]', 120000, false, () =>
+      console.log('checking home'),
+    );
+};
+
+export const HappyPath = (browser: NightwatchBrowser) => {
+  browser
+    .url('http://home.localhost')
     .waitForElementPresent('h1[data-testid="eduvault-title"]', 120000, false)
     .assert.containsText('h1', 'EDUVAULT')
     .click('button[data-testid="button-try-now"]')
-  .expect.element('a[data-testid="link-example"]').is.visible
-  browser.click('a[data-testid="link-example"]')  // navigates to example
-  .waitForElementVisible('img.eduvault-button', 50000)
-  .waitForElementPresent('a[href^="http://app.localhost"]', 100000);
+    .expect.element('a[data-testid="link-example"]').is.visible;
+  browser
+    .click('a[data-testid="link-example"]') // navigates to example
+    .waitForElementVisible('img.eduvault-button', 50000)
+    .waitForElementPresent('a[href^="http://app.localhost"]', 100000);
 
   // renavigate manually to avoid ERR_TOO_MANY_REDIRECTS
   browser
     .url('http://example.localhost')
     .waitForElementPresent('a[href^="http://app.localhost"]', 100000)
-  .click('img.eduvault-button')
+    .click('img.eduvault-button')
 
-  // navigates to app
-  .waitForElementPresent('input[type=email]', 200000)
-  .setValue('input[type=email]', 'example@somewhere.com')
-  .setValue('input[type=password]', 'Password123')
-  .click('button[type=submit]')
-  // logs in
-  .waitForElementPresent('h2[data-testid="deck-title"]', 200000)
-  // starts DB and shows default deck
-  .expect.element('h2[data-testid="deck-title"]').text.to.contain('Default Deck')
+    // navigates to app
+    .waitForElementPresent('input[type=email]', 200000)
+    .setValue('input[type=email]', 'example@somewhere.com')
+    .setValue('input[type=password]', 'Password123')
+    .click('button[type=submit]')
+    // logs in
+    .waitForElementPresent('h2[data-testid="deck-title"]', 200000)
+    // starts DB and shows default deck
+    .expect.element('h2[data-testid="deck-title"]')
+    .text.to.contain('Default Deck');
 };

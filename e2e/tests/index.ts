70485c18553f8e5b36821fcc437a924d369c30ff
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-07-15 13:12:05 +0800

    diff wait strategy

--- e2e/tests/index.ts
+++ e2e/tests/index.ts
@@ -1,8 +1,8 @@
 import { NightwatchAPI, NightwatchBrowser, NightwatchCallbackResult } from 'nightwatch';
 const serverTestEnv = process.env.TEST === '1';
 console.log({ serverTestEnv, 'process.env.TEST': process.env.TEST });
-
-export const Base = (browser: NightwatchBrowser) => {
+import waitOn from 'wait-on';
+export const Base = async (browser: NightwatchBrowser) => {
   // check nightwatch is working
   // browser.url('https://google.com').waitForElementPresent('body').assert.titleContains('Google');
   // check base url working
@@ -23,47 +23,67 @@ export const Base = (browser: NightwatchBrowser) => {
   // },
   // navigate to app, example, and api.
 
-  browser
-    .url('http://example.localhost')
-    .waitForElementVisible('img.eduvault-button', 50000, false, () =>
-      console.log('checking example'),
-    )
-  browser
-    .url('http://app.localhost')
-    .waitForElementPresent('input[type=email]', 50000, false, () => console.log('checking app'));
-  browser
-    .url('http://home.localhost')
-    .waitForElementPresent('h1[data-testid="eduvault-title"]', 120000, false, () =>
-      console.log('checking home'),
-    );
+  try {
+    await waitOn({
+      resources: ['http://localhost', 'http://api.localhost/ping'],
+      timeout: 5000,
+      verbose: true,
+      delay: 1000,
+    });
+    browser
+      .url('http://home.localhost')
+      .waitForElementPresent('h1[data-testid="eduvault-title"]', 120000, false, () =>
+        console.log('checking home'),
+      );
+    browser
+      .url('http://example.localhost')
+      .waitForElementVisible('img.eduvault-button', 50000, false, () =>
+        console.log('checking example'),
+      );
+    browser
+      .url('http://app.localhost')
+      .waitForElementPresent('input[type=email]', 50000, false, () => console.log('checking app'));
+  } catch (err) {
+    browser.assert.visible('.non_existing');
+  }
 };
 
-export const HappyPath = (browser: NightwatchBrowser) => {
-  browser
-    .url('http://home.localhost')
-    .waitForElementPresent('h1[data-testid="eduvault-title"]', 120000, false)
-    .assert.containsText('h1', 'EDUVAULT')
-    .click('button[data-testid="button-try-now"]')
-    .expect.element('a[data-testid="link-example"]').is.visible;
-  browser
-    .click('a[data-testid="link-example"]') // navigates to example
-    .waitForElementVisible('img.eduvault-button', 50000)
-    .waitForElementPresent('a[href^="http://app.localhost"]', 100000);
+export const HappyPath = async (browser: NightwatchBrowser) => {
+  try {
+    await waitOn({
+      resources: ['http://localhost', 'http://api.localhost/ping'],
+      timeout: 5000,
+      verbose: true,
+      delay: 1000,
+    });
+    browser
+      .url('http://home.localhost')
+      .waitForElementPresent('h1[data-testid="eduvault-title"]', 120000, false)
+      .assert.containsText('h1', 'EDUVAULT')
+      .click('button[data-testid="button-try-now"]')
+      .expect.element('a[data-testid="link-example"]').is.visible;
+    browser
+      .click('a[data-testid="link-example"]') // navigates to example
+      .waitForElementVisible('img.eduvault-button', 50000)
+      .waitForElementPresent('a[href^="http://app.localhost"]', 100000);
 
-  // renavigate manually to avoid ERR_TOO_MANY_REDIRECTS
-  browser
-    .url('http://example.localhost')
-    .waitForElementPresent('a[href^="http://app.localhost"]', 100000)
-    .click('img.eduvault-button')
+    // renavigate manually to avoid ERR_TOO_MANY_REDIRECTS
+    browser
+      .url('http://example.localhost')
+      .waitForElementPresent('a[href^="http://app.localhost"]', 100000)
+      .click('img.eduvault-button')
 
-    // navigates to app
-    .waitForElementPresent('input[type=email]', 200000)
-    .setValue('input[type=email]', 'example@somewhere.com')
-    .setValue('input[type=password]', 'Password123')
-    .click('button[type=submit]')
-    // logs in
-    .waitForElementPresent('h2[data-testid="deck-title"]', 200000)
-    // starts DB and shows default deck
-    .expect.element('h2[data-testid="deck-title"]')
-    .text.to.contain('Default Deck');
+      // navigates to app
+      .waitForElementPresent('input[type=email]', 200000)
+      .setValue('input[type=email]', 'example@somewhere.com')
+      .setValue('input[type=password]', 'Password123')
+      .click('button[type=submit]')
+      // logs in
+      .waitForElementPresent('h2[data-testid="deck-title"]', 200000)
+      // starts DB and shows default deck
+      .expect.element('h2[data-testid="deck-title"]')
+      .text.to.contain('Default Deck');
+  } catch (err) {
+    browser.assert.visible('.non_existing');
+  }
 };
